82a797b3c86422b8376b79d4de5bb4c7cd529ab4
Josué Felipe <josueflx1999@gmail.com>
2020-12-12 18:48:49 -0300

    --- Many-to-Many association with JoinTable ---
    	--- Product.java ---
    * Removido anotação Transient
    * Adicionado anotação JoinTable que define como será feito a associação
      entre as tabelas product e category e seus elementos
    
    	--- TestConfig.java ---
    * Inserindo dados de categorias correspondentes a cada produto
    * Inserindo os dados informados no banco de dados de teste

--- src/main/java/com/reign/course/entities/Product.java
+++ src/main/java/com/reign/course/entities/Product.java
@@ -8,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -25,9 +28,13 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 	
-	// Associações
-	@Transient
-	private Set<Category> categories = new HashSet<>(); // conjunto que impede que duplique valores
+	// ASSOCIAÇÕES
+	//@Transient utilizado para que o jpa não reconheça
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id")) 
+	private Set<Category> categories = new HashSet<>(); // CONJUNTO QUE IMPEDE QUE DUPLIQUE VALORES
 	
 	public Product() {}
 
