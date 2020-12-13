3c5d609eeb32727fe267b99ca84e2c362a9282b1
Josué Felipe <josueflx1999@gmail.com>
2020-12-13 17:42:08 -0300

    --- Payment One-to-One association ---
    	--- Payment.java ---
    * Criando classe
    * Implementando Serializable
    * Definindo atributos
    * Implementando composição de Order
    * Implementando metodos construtores com e sem parametros
    * Implementando metodos get e set
    * Implementando metodos HashCode e Equals
    * Adicionando anotações para o JPA
    
    	--- Order.java ---
    * Implementando composição de Payment
    * Implementando metodos get e set para a composição
    * Adicionando anotação JPA para a composição
    
    	--- TestConfig.java ---
    * Instanciando objeto do tipo Payment
    * Inserindo dados banco de dados de teste

--- src/main/java/com/reign/course/entities/Order.java
+++ src/main/java/com/reign/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -38,6 +40,10 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	// ASSOCIAÇÃO
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL) // MAPEANDO A RELAÇÃO ENTRE AS ENTIDADES PARA QUE TENHAM O MESMO ID
+	private Payment payment;
 	
 	public Order() {}
 
@@ -78,7 +84,13 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
-	
+	public Payment getPayment() {
+		return payment;
+	}
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
